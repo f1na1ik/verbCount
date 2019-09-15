1a6aef2848fc647bdf7ac3bb8020a59d587e1ef1
f1na1ik <maxsim119@yandex.ru>
2019-09-15 23:55:34 +0300

    prog ready

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,8 @@ function processing() {
     let countVerbs = 0;
     let j = 0;
 
-    let textWords = document.getElementById('inputText').innerHTML;
-    console.log(textWords);
+    let textWords = document.getElementById('inputText').value;
+    // console.log(textWords);
     let arrayWords = textWords.split(',').join('').split('.').join('')
         .split('(').join('').split(')').join('').split(' – ').join('').split('\n').join(' ')
         .split(' ');
@@ -14,8 +14,9 @@ function processing() {
     Az.Morph.init('../node_modules/az/dicts', function() {
         for (let i = 0; arrayWords.length > i; i++) {
             let parses = Az.Morph(arrayWords[i]);
-            console.log(parses[0].tag.VERB);
-            if (parses[0].tag.VERB) {
+            // console.log(parses);
+            // console.log(parses[0].tag.VERB);
+            if (parses[0].tag.VERB || parses[0].tag.INFN) {
                 allWordsArray[j] = arrayWords[i];
                 j++;
                 // console.log(arrayWords[i]);
@@ -23,8 +24,8 @@ function processing() {
             }
         }
 
-        console.log('Всего глаголов: '+countVerbs);
-        console.log(allWordsArray.join('\n'));
+        // console.log('Всего глаголов: '+countVerbs);
+        // console.log(allWordsArray.join('\n'));
 
         document.getElementById('outputText').innerHTML = allWordsArray.join('\n');
         document.getElementById('verbsCount').innerHTML = '&nbsp' + j ;
